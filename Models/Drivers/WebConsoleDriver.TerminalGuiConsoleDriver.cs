43def84b027662ff4c795255543e24fe4d8e302b
Jessica Mulein <jessica@brightchain.org>
2022-03-22 16:18:31 -0700

    simplify

--- Models/Drivers/WebConsoleDriver.TerminalGuiConsoleDriver.cs
+++ Models/Drivers/WebConsoleDriver.TerminalGuiConsoleDriver.cs
@@ -294,6 +294,7 @@ public partial class WebConsoleDriver
             }
 
             var task = this.ConsoleWeb.DrawUpdatesToCanvas(
+
                 buffer: this.Contents,
                 firstRender: firstRender);
             // ReSharper disable once HeapView.ObjectAllocation.Evident
