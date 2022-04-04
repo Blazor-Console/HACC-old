3e51d88a0d43ba71e85e97b506b067e6c286ff3c
BDisp <bd.bdisp@gmail.com>
2022-04-04 19:12:03 +0100

    Adding more methods to the Web.Application

--- Applications/WebApplication.cs
+++ Applications/WebApplication.cs
@@ -8,12 +8,14 @@ namespace HACC.Applications;
 public class WebApplication
 {
     private Application.RunState state;
+    private bool wait = true;
+    private bool _initialized;
 
     public readonly WebConsoleDriver WebConsoleDriver;
     public readonly WebMainLoopDriver WebMainLoopDriver;
     public readonly WebConsole WebConsole;
 
-    public WebApplication(WebConsoleDriver webConsoleDriver, 
+    public WebApplication(WebConsoleDriver webConsoleDriver,
         WebMainLoopDriver webMainLoopDriver, WebConsole webConsole)
     {
         this.WebConsoleDriver = webConsoleDriver;
@@ -26,25 +28,76 @@ public class WebApplication
 
     private void WebConsole_RunIterationNeeded()
     {
-        Application.RunIteration(state, true, false);
+        Application.RunIteration(state, wait, false);
     }
 
     public virtual void Init()
     {
+        if (_initialized) return;
+
         Application.Init(
             driver: this.WebConsoleDriver,
             mainLoopDriver: this.WebMainLoopDriver);
+        _initialized = true;
     }
 
-    public virtual void Run()
+    public virtual void Run(Func<Exception, bool> errorHandler = null)
     {
-        _ = Task.Run(() => state = Application.Begin(toplevel: Application.Top));
-        _ = Task.Run(() => WebConsoleDriver.firstRender = false);
-        _ = Task.Run(Application.Refresh);
+        Run(Application.Top, errorHandler);
+    }
+
+    public virtual void Run<T>(Func<Exception, bool> errorHandler = null) where T : Toplevel, new()
+    {
+        if (_initialized && Application.Driver != null)
+        {
+            var top = new T();
+            if (top.GetType().BaseType != typeof(Toplevel))
+            {
+                throw new ArgumentException(top.GetType().BaseType.Name);
+            }
+            Run(top, errorHandler);
+        }
+        else
+        {
+            Init();
+            Run(Application.Top, errorHandler);
+        }
+    }
+
+    public virtual void Run(Toplevel view, Func<Exception, bool> errorHandler = null)
+    {
+        try
+        {
+            if (!_initialized)
+                Init();
+
+            _ = Task.Run(() => state = Application.Begin(toplevel: view ?? Application.Top));
+            _ = Task.Run(() => WebConsoleDriver.firstRender = false);
+            _ = Task.Run(Application.Refresh);
+            _ = Task.Run(() => Application.RunIteration(state, wait, true));
+        }
+        catch (Exception error)
+        {
+            if (errorHandler == null)
+            {
+                throw;
+            }
+            if (!errorHandler(error))
+                Shutdown();
+        }
+    }
+
+    public virtual void End(Application.RunState runState = null)
+    {
+        if (!_initialized) return;
+
+        if (runState != null || state != null)
+            Application.End(runState ?? state);
     }
 
     public virtual void Shutdown()
     {
         Application.Shutdown();
+        _initialized = false;
     }
 }
